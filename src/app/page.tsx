e938407dbcfe3b683d015299a0be57e381771d46
12haa <12hadoto@gmail.com>
2024-02-05 15:15:17 +0330

    Add navRef-Escale Onlick for Menu Items TO Close

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="p-y-20">
-          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-10">
             {perks.map((p, i) => (
               <div
                 key={p.name}
---
+++ src/components/NavItems.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "@/components/NavItem";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  return (
+    <div className="flex  h-full gap-4  " ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+        const isOpen = activeIndex === i;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen} 1 22 44
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
